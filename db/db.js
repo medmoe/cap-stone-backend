5888efde59b4f986c487feba61be2182152d5375
medmoe <med.seffah@gmail.com>
2021-01-25 17:36:04 -0500

    set up the server

--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 const { name } = require('../package.json');
 
-// Initialize database with Sequelize
+// Initialize database with Sequelize . put your username and password for database
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${name}`,
+  process.env.DATABASE_URL || `postgres:postgres:12345//localhost:5432/${name}`,
   {
     logging: false,
   }
