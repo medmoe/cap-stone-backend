bd2029d7666e6908d0019f08644c83ef92b3b8ca
medmoe <med.seffah@gmail.com>
2021-01-25 21:39:44 -0500

    added raw data

--- seed.js
+++ seed.js
@@ -1,6 +1,7 @@
 const db = require('./db');
 
 const { User } = require('./db/models');
+const Recipe = require('./db/models/recipe');
 
 
 
@@ -35,8 +36,28 @@ const seedDatabase = async() => {
     lastName: "Leaves",
     email: "sleaves4@blogspot.com",
     password: "oGulIieavr"
+   }),
+   Recipe.create({
+    name: "Oriental Knight's-spur",
+    description: "Suspendisse potenti. In eleifend quam a odio.  sollicitudin vitae, consectetuer eget, rutrum at, lorem."
+   }),
+   Recipe.create({
+    name: "Resinleaf Brickellbush",
+    description: "Maecenas tristique, est et tempus semper,ci lu posuere metus vitae ipsum. Aliquam non mauris."
+   }),
+   Recipe.create({
+    name: "Rapp's Ramonia",
+    description: "Integer ac leo. Pellentesque ultrices mitsuscipit ligula in lacus."
+   }),
+   Recipe.create({
+    name: "Western Azalea",
+    description: "Pellentesque at nulla. Suspendisse poturus eu magna vulputate luctus."
+   }),
+   Recipe.create({
+    name: "Mexican Orange",
+    description: "Duis aliquam convallis nunc. Proin e nonummy. Integer non velit."
    })
-  ]);
+  ])
 }
 
 module.exports = seedDatabase;
