d4e61a77c2e7fab4df2ceba3e06f2976cb5cc4a1
medmoe <med.seffah@gmail.com>
2021-01-25 20:15:44 -0500

    connect to db

--- db/models/index.js
+++ db/models/index.js
@@ -1,7 +1,9 @@
 const Player = require('./player');
+const User = require('./user');
 
 //ASSOICATIONS GO HERE -- Read more at https://sequelize.org/master/manual/assocs.html
 
 module.exports = {
   Player,
+  User,
 };
