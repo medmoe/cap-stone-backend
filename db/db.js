53bf5aa51e344f01eb600efe367761383f59bdd0
medmoe <med.seffah@gmail.com>
2021-01-31 08:16:26 -0500

    updated database config for deployement

--- db/db.js
+++ db/db.js
@@ -3,11 +3,17 @@ const { name } = require("../package.json");
 
 // Initialize database with Sequelize . put your username and password for database
 const db = new Sequelize(
-	process.env.DATABASE_URL ||
-		`postgres://postgres:12345@localhost:5432/${name}`,
-	{
-		logging: false,
+	process.env.DATABASE_URL,{
+		dialect: 'postgres',
+		protocol: 'postgres',
+		dialectOptions: {
+			ssl: {
+				sslmode: 'require',
+				rejectUnauthorized: false
+			}
+		}
 	}
+		//`postgres://postgres:12345@localhost:5432/${name}`,
 );
 
 module.exports = db;
