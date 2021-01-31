ece7758d211d9fc9f5a322a480753d052d1af565
mohammed <med.seffah@gmail.com>
2021-01-31 17:10:57 -0500

    changed to netfly link

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const seedDatabase = require('./seed.js');
 
 //CORS!
 const corsOptions = {
-	origin: "http://localhost:3000",
+	origin: "http://recipe-board-app.netlify.app",
 	credentials: true,
 	optionsSuccessStatus: 200,
 };
