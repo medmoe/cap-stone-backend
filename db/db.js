47e76a67fa60c7a2658c46f7ca50fcc81e1e7678
ruimin <ruiminxu9478@gmail.com>
2021-01-30 20:47:13 -0500

    bug fixing

--- db/db.js
+++ db/db.js
@@ -1,14 +1,13 @@
-const Sequelize = require('sequelize');
-const { name } = require('../package.json');
+const Sequelize = require("sequelize");
+const { name } = require("../package.json");
 
 // Initialize database with Sequelize . put your username and password for database
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://postgres:12345@localhost:5432/${name}`,
-  {
-    logging: false,
-  }
+	process.env.DATABASE_URL ||
+		`postgres://postgres:Imthebest1!@localhost:5432/${name}`,
+	{
+		logging: false,
+	}
 );
 
-
 module.exports = db;
-
