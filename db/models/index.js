1fc12d212dea22490fae4ee4a073c6e656ffacab
Binqing Zheng <bqz2101@gmail.com>
2021-01-30 20:01:47 -0500

    testing

--- db/models/index.js
+++ db/models/index.js
@@ -1,14 +1,8 @@
 const User = require('./user');
 const Recipe = require('./recipe');
 
-//ASSOICATIONS GO HERE -- Read more at https://sequelize.org/master/manual/assocs.html
-
-User.belongsToMany(Recipe , {through: '"user_recipe", foreignKey: "user_id"});
+User.belongsToMany(Recipe , {through: "user_recipe", foreignKey: "user_id"});
 Recipe.belongsToMany(User , {through: "user_recipe", foreignKey: "recipe_id"});
-
-
-
-
 
 module.exports = {
   User,
