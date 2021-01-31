47d8319ca034e31d305d57107f8c925062aeb629
mohammed <med.seffah@gmail.com>
2021-01-31 11:57:29 -0500

    added a route to seed the database

--- api/recipe_controller.js
+++ api/recipe_controller.js
@@ -7,7 +7,56 @@ const Sequelize = require("sequelize");
 
 const models = require("../db/models");
 const request = require("request");
+let rps = [];
+//if you want to find more recipes just add letters to the variable 'word'
+let word = "abc";
+for (let i = 0; i < word.length; i++) {
+    request(`https://www.themealdb.com/api/json/v1/1/search.php?f=${word.charAt(i)}`, (error, response, body) => {
+        if (error) {
+            console.log(error);
+        } else {
+            JSON.parse(body).meals.forEach(element => {
+                let ingredients = [];
+                let { strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9 } = element;
+                ingredients.push(strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9);
+                let recipe = {
+                    name: element.strMeal,
+                    category: element.strCategory,
+                    area: element.strArea,
+                    instructions: element.strInstructions,
+                    image: element.strMealThumb,
+                    ingredients: ingredients
+                }
 
+                rps.push(recipe);
+            })
+
+        }
+    });
+}
+
+// add recipes to database
+router.post("/", async (req,res,next) => {
+	try{
+		rps.forEach(async (element) => {
+              //create a recipe object
+              let r = {
+                  name: element.name,
+                  category: element.category,
+                  area: element.area,
+                  instructions: element.instructions,
+                  all_ingredients: element.ingredients.join(','),
+                  image: element.image,
+              }
+              //store the recipe object in recipes table
+              let recipe = await Recipe.create(r);
+          })
+
+	}catch(error){
+		consol.log(error);
+	}
+
+})
 ////A route to fetch all recipes
 router.get("/", async (req, res, next) => {
 	try {
--- index.js
+++ index.js
@@ -103,47 +103,6 @@ const syncDb = () => {
 };
 //Run server and sync DB
 syncDb();
-let rps = [];
-//if you want to find more recipes just add letters to the variable 'word'
-let word = "abc";
-for (let i = 0; i < word.length; i++) {
-    request(`https://www.themealdb.com/api/json/v1/1/search.php?f=${word.charAt(i)}`, (error, response, body) => {
-        if (error) {
-            console.log(error);
-        } else {
-            JSON.parse(body).meals.forEach(element => {
-                let ingredients = [];
-                let { strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9 } = element;
-                ingredients.push(strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9);
-                let recipe = {
-                    name: element.strMeal,
-                    category: element.strCategory,
-                    area: element.strArea,
-                    instructions: element.strInstructions,
-                    image: element.strMealThumb,
-                    ingredients: ingredients
-                }
-
-                rps.push(recipe);
-            })
-
-        }
-    });
-}
-          rps.forEach(async (element) => {
-              //create a recipe object
-              let r = {
-                  name: element.name,
-                  category: element.category,
-                  area: element.area,
-                  instructions: element.instructions,
-                  all_ingredients: element.ingredients.join(','),
-                  image: element.image,
-              }
-              //store the recipe object in recipes table
-              let recipe = await Recipe.create(r);
-          })
-          
 serverRun();
 
 module.exports = app;
