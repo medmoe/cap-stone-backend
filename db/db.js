c833b6ea3bb55d190922d621857457a3e0dc46e3
mohammed <med.seffah@gmail.com>
2021-01-31 10:06:49 -0500

    updated db.js

--- db/db.js
+++ db/db.js
@@ -3,9 +3,16 @@ const { name } = require("../package.json");
 
 // Initialize database with Sequelize . put your username and password for database
 const db = new Sequelize(
-	process.env.DATABASE_URL || `postgres://postgres:12345@localhost:5432/${name}`,
+	process.env.DATABASE_URL,
 	{
-		logging: false,
+		dialect: 'postgres',
+		protocol: 'postgres',
+		dialectOptions: {
+			ssl: {
+				sslmode: 'require',
+				rejectUnauthorized: false
+			}
+		}
 	}
 );
 
