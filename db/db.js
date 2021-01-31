f6c96ad75f1c9653163c3466cd42b4dcb8b4bfc1
mohammed <med.seffah@gmail.com>
2021-01-31 09:53:07 -0500

    fix db.js

--- db/db.js
+++ db/db.js
@@ -3,17 +3,10 @@ const { name } = require("../package.json");
 
 // Initialize database with Sequelize . put your username and password for database
 const db = new Sequelize(
-	process.env.DATABASE_URL,{
-		dialect: 'postgres',
-		protocol: 'postgres',
-		dialectOptions: {
-			ssl: {
-				sslmode: 'require',
-				rejectUnauthorized: false
-			}
-		}
+	process.env.DATABASE_URL || `postgres://postgres:12345@localhost:5432/${name}`,
+	{
+		logging: false,
 	}
-		//`postgres://postgres:12345@localhost:5432/${name}`,
 );
 
 module.exports = db;
